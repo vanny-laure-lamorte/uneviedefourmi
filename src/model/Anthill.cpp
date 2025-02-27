928539e48106093c7a3737ca939db543852baa4f
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-02-27 17:53:58 +0100

    🏗️  <update>[all files] Move core classes to model directory for better organization

---
+++ src/model/Anthill.cpp
@@ -0,0 +1,132 @@
+#include "Anthill.hpp"
+
+Anthill::Anthill(json anthillData) {
+    for (auto& roomData : anthillData["rooms"].items()) {
+        string roomName = roomData.key();
+        int capacity = (roomData.value().is_null()) ? -1 : roomData.value().get<int>();
+        rooms.push_back(Room(roomName, capacity));
+    }
+
+    Sv = findRoomByName("SV");
+    Sd = findRoomByName("SD");
+
+    int antCount = anthillData["ants"];
+    for (int i = 0; i < antCount; ++i) {
+        ants.push_back(Ant(i + 1, Sv));
+    }
+
+    for (auto& connection : anthillData["connections"]) {
+        Room* fromRoom = findRoomByName(connection["from"]);
+        Room* toRoom = findRoomByName(connection["to"]);
+        fromRoom->addTunnels(toRoom);
+    }
+
+    displayAnthillInfo();
+    displayAntLocationStepByStep();
+}
+
+void Anthill::displayAnthillInfo() {
+
+    mainMenu.displayRectangleWithTitle(" ANTHILL INFORMATION ");
+
+    cout << ">Rooms Information " << endl;
+    for (auto& room : rooms) {
+        cout << "Room: " << room.getName() 
+             << ", Capacity: " << (room.getCapacity() == -1 ? "Unlimited" : to_string(room.getCapacity())) 
+             << endl;
+    }
+
+    cout << "\n>Start Room: " << Sv->getName() << endl;
+    cout << ">End Room: " << Sd->getName() << endl;
+
+    cout << "\n>Ants Information " << endl;
+    for (auto& ant : ants) {
+        cout << "Ant " << ant.getId() << ", Start Room: " << ant.getStartRoom()->getName() << endl;
+    }
+
+    cout << "\n>Connections Information: " << endl;
+    for (auto& room : rooms) {
+        for (auto& tunnel : room.getTunnels()) {
+            cout << "Room: " << room.getName() 
+                 << " is connected to: " << tunnel->getName() << endl;
+        }
+    }
+}
+
+Room* Anthill:: findRoomByName(const string& roomName) {
+    for (auto& room : rooms) {
+        if (room.name == roomName) {
+            return &room;
+        }
+    }
+    return nullptr;
+}
+
+void Anthill::moveAnts() {
+    for (auto& ant : ants) {
+        for (auto& tunnel : ant.currentRoom->tunnels) {
+            if (canMoveToRoom(tunnel)) {
+                ant.moveAntToNewLocation(tunnel);
+                cout << "Ant " << ant.id << " moved to room " << ant.currentRoom->name << endl;
+            }
+        }
+    }
+}
+
+bool Anthill::canMoveToRoom(Room* room) {
+    if (room->capacity == -1 || getAntsInRoom(room) < room->capacity) {
+        return true; 
+    }
+    return false; 
+}
+
+int Anthill::getAntsInRoom(Room* room) {
+    int count = 0;
+    for (auto& ant : ants) {
+        if (ant.currentRoom == room) {
+            count++;
+        }
+    }
+    return count;
+}
+
+void Anthill::displayAntLocationStepByStep() {
+
+    int step = 1;
+
+    while (true) {
+        mainMenu.displayRectangleWithTitleAndVariable("ANT MOVEMENT STEP ", step);
+        for (auto& ant : ants) {
+            cout << "Ant " << ant.getId() << " is in room " << ant.getStartRoom()->getName() << endl;
+        }
+
+        cout << "\n>Press Enter to move ants to the next step ";
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        bool allAtEnd = true; 
+        for (auto& ant : ants) {
+            bool moved = false;
+            for (auto& tunnel : ant.getStartRoom()->getTunnels()) {
+                if (canMoveToRoom(tunnel)) {
+                    ant.moveAntToNewLocation(tunnel);  
+                    moved = true;
+                    break;
+                }
+            }
+
+            if (moved) {
+                cout << "Ant " << ant.getId() << ">moved to next room " << ant.getStartRoom()->getName() << endl;
+            } else {
+                cout << "Ant " << ant.getId() << "> Cannot move (no available tunnels or capacity full).\n";
+                allAtEnd = false;  
+            }
+        }
+        step++;
+
+        if (allAtEnd) {
+            cout << "\nAll ants have reached the end room!" << endl;
+            break;  
+        }
+    }
+}
+
