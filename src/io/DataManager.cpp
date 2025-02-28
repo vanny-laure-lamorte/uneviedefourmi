ea23a71398fa4e4b371ce771a60b8d25749493f9
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2025-02-28 15:08:38 +0100

    🎉 <feat>[DataManager] Implement loadAnthillFromJson method for loading anthill data from JSON

--- src/io/DataManager.cpp
+++ src/io/DataManager.cpp
@@ -28,6 +28,75 @@ json DataManager::getAnthillData(int anthillNumber) {
     return data;
 }
 
+Anthill DataManager::loadAnthillFromJson(int anthillNumber)
+{
+    try
+    {
+        json anthillData = getAnthillData(anthillNumber);
+        if (anthillData.is_null())
+        {
+            throw std::runtime_error("Anthill data is empty or invalid.");
+        }
+
+        std::vector<Room> rooms;
+        Room* vestibule = nullptr;
+        Room* dortoir = nullptr;
+
+        for (const auto& [roomName, capacity] : anthillData["rooms"].items())
+        {
+            Room newRoom(roomName, capacity);
+            rooms.push_back(newRoom);
+
+            if (roomName == "SV")
+                vestibule = &rooms.back();
+            else if (roomName == "SD")
+                dortoir = &rooms.back();
+        }
+
+        if (!vestibule || !dortoir)
+        {
+            throw std::runtime_error("Missing essential rooms (SV or SD).");
+        }
+
+        for (const auto& connection : anthillData["connections"])
+        {
+            std::string from = connection["from"];
+            std::string to = connection["to"];
+
+            Room* fromRoom = nullptr;
+            Room* toRoom = nullptr;
+
+            for (auto& room : rooms)
+            {
+                if (room.getName() == from)
+                    fromRoom = &room;
+                if (room.getName() == to)
+                    toRoom = &room;
+            }
+
+            if (fromRoom && toRoom)
+            {
+                fromRoom->addTunnels(toRoom);
+            }
+        }
+
+        std::vector<Ant> ants;
+        int numberOfAnts = anthillData["ants"];
+        for (int i = 1; i <= numberOfAnts; i++)
+        {
+            ants.emplace_back(i, vestibule);
+        }
+
+        return Anthill(rooms, ants, vestibule, dortoir, 0);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error loading anthill: " << e.what() << std::endl;
+        return Anthill({}, {}, nullptr, nullptr, 0);
+    }
+}
+
+
 // void GraphManager::loadGraph(std::string graphPath) {
 //     std::unordered_map<std::string, std::vector<std::string>> nodes;
 //     std::cout << "Loading graph..." << std::endl;
